38000bcdc671d9e2d09861cd112b6bf2f62fdbfd
Debobrata Paull <125825050+DPaul-11@users.noreply.github.com>
2024-03-30 18:18:40 +0530

    Update Pcs1.java

--- Pcs1.java
+++ Pcs1.java
@@ -1,3 +1,41 @@
+// Problem
+// You are given the sequence of Nucleotides of one strand of DNA through a string SS of length NN. SS contains the character A, T, C, A, T, C, and GG only.
+//   knows that:
+// AA is complementary to TT.
+// TT is complementary to AA.
+// CC is complementary to GG.
+// GG is complementary to CC.
+// Using the string SS, determine the sequence of the complementary strand of the DNA.
+// Input Format
+// First line will contain TT, number of test cases. Then the test cases follow.
+// First line of each test case contains an integer NN - denoting the length of string SS.
+// Second line contains NN characters denoting the string SS.
+// Output Format
+// For each test case, output the string containing NN characters - sequence of nucleotides of the complementary strand.
+// Constraints
+// 1≤T≤100
+// 1≤N≤100
+// S contains A, T, C, and G only
+// Sample 1:
+// Input
+// 4
+// 4
+// ATCG
+// 4
+// GTCC
+// 5
+// AAAAA
+// 3
+// TAC
+
+// Output
+
+// TAGC
+// CAGG
+// TTTTT
+// ATG
+
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
